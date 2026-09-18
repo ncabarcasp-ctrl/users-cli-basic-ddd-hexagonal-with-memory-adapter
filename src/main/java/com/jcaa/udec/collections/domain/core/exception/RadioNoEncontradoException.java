445f906743d05ca5ba6a22168157667ae5810c26
NAT <ncabarcasp@unicartagena.edu.co>
2026-09-18 00:32:24 -0500

    feat: add Read ports and RadioNoEncontradoException

---
+++ src/main/java/com/jcaa/udec/collections/domain/core/exception/RadioNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.jcaa.udec.collections.domain.core.exception;
+
+public class RadioNoEncontradoException extends RuntimeException {
+    public RadioNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
